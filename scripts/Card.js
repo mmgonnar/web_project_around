101ab282632ab5971d487a431917a1af28d5c6c7
mmgonnar <gn.mariela@gmail.com>
2024-01-16 17:16:56 -0600

    feat(javascript):Empty commit

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,9 @@
 //Creates cards
+/* function createCard(title, link) {
+  cardTitle.innerText = title;
+  cardImage.src = link;
+} */
+
 export default class Card {
   constructor(name, link, templateSelector) {
     this._name = name;
@@ -12,31 +17,69 @@ export default class Card {
       .content.querySelector(".card")
       .cloneNode(true);
 
-      const cardImage = card.querySelector(".card__image");
-      const cardTitle = card.querySelector(".card__title");
-      const popupImageElement = document.querySelector(".popup__element");
-      cardTitle.innerText = title;
-      cardImage.src = link;
-
-
     return templateSelector;
   }
 
-  setEventListeners() {
-    buttonImage.addEventListener("click", function () {
+  openPopup() {
+    const cardImage = card.querySelector(".card__image");
+    const cardTitle = card.querySelector(".card__title");
+    const popupImageElement = document.querySelector(".popup__element");
+  }
+
+  _setEventListeners(cardElement) {
+    const buttonLike = cardElement.querySelector(".button_like");
+    buttonLike.addEventListener("click", () => {
+      buttonLike.classList.toggle("liked");
+    });
+
+    const buttonDelete = cardElement.querySelector(".button_delete");
+    buttonDelete.addEventListener("click", () => {
+      const card = buttonDelete.closest(".card");
+      card.remove();
+    });
+
+    const buttonImage = cardElement.querySelector(".card__image");
+    buttonImage.addEventListener("click", () => {
       popupImageElement.src = cardImage.src;
       popupButtonSwitch(popupImage);
       popupImageElement.setAttribute("alt", title);
+
+      popupImageTitle.textContent = cardTitle.textContent;
+    });
   }
+
+  closePopup() {}
+
+  /**
+    generateCard() {
+      this._templateSelector = this.getTemplate();
+      this.setEventListeners();
+      return this.?
+    }
+  */
 
   generateCard() {
-    this._templateSelector = this.getTemplate();
-    this._setEventListeners();
-    return this._?
+    //
+    const cardElement = this.getTemplate();
+
+    // Esta es una etiqueta <img />
+    const cardImage = cardElement.querySelector(".card__image");
+
+    // Esta es una etiqueta <h6></h6>
+    const cardTitle = cardElement.querySelector(".card__title");
+
+    // Desarrollo de atributos en <img src="" alt="" />
+    cardImage.src = this._link;
+    cardImage.alt = this._name;
+
+    // <h6>{this._name}</h6>
+    cardTitle.textContent = this._name;
+
+    this._setEventListeners(cardElement);
+
+    return cardElement;
   }
 }
-
-const card = new Card(item.name, item.link, "#cards-template");
 
 //
 
