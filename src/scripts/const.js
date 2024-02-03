f3d978d9fafa0cdc0c84deaab20ac67597c16c82
mmgonnar <gn.mariela@gmail.com>
2024-02-03 11:30:50 -0600

    build(html): Changed syntaxis of img's path

---
+++ src/scripts/const.js
@@ -0,0 +1,45 @@
+export const buttonEdit = document.querySelector(".button_edit");
+export const popupProfile = document.querySelector(".popup_edit");
+export const buttonAdd = document.querySelector(".button_add");
+export const profileName = document.querySelector(".profile__name");
+export const profileJob = document.querySelector(".profile__job");
+export const profileForm = document.querySelector(".popup__form");
+export const addForm = document.querySelector(".popup__form_add");
+export const popupImageElement = document.querySelector(".popup__element");
+export const popupProfileSelector = ".popup_edit";
+export const popupAddSelector = ".popup_add";
+export const popupImageSelector = ".popup_image";
+
+import Chicago from "../images/places/chicago.png";
+import Arizona from "../images/places/arizona.png";
+import Miami from "../images/places/miami.png";
+import SantaMonica from "../images/places/santa_monica.png";
+import LasVegas from "../images/places/vegas.png";
+import Yellowstone from "../images/places/yellowstone_np.png";
+
+export const initialCards = [
+  {
+    name: "Chicago",
+    link: Chicago,
+  },
+  {
+    name: "Arizona",
+    link: Arizona,
+  },
+  {
+    name: "Miami",
+    link: Miami,
+  },
+  {
+    name: "Santa Monica",
+    link: SantaMonica,
+  },
+  {
+    name: "Las Vegas",
+    link: LasVegas,
+  },
+  {
+    name: "Yellowstone",
+    link: Yellowstone,
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,68 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  devtool: "inline-source-map",
-  entry: {
-    app: "./src/index.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    filename: "js/[name].js",
-    publicPath: "",
-    assetModuleFilename: "images/[name][ext][query]",
-  },
-  target: ["web", "es5"],
-  stats: { children: true },
-  mode: "development",
-  devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
-    compress: true,
-    port: 8080,
-    open: true,
-  },
-  module: {
-    rules: [
-      // esto es un array de reglas
-      // añádele un objeto que contenga reglas para Babel
-      {
-        // una expresión regular que busca todos los archivos js
-        test: /\.js$/,
-        // todos los archivos deben ser procesados por babel-loader
-        loader: "babel-loader",
-        // excluye la carpeta node_modules, no necesitamos procesar archivos en ella
-        exclude: "/node_modules/",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
+    devtool: 'inline-source-map',
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'js/[name].js',
+        publicPath: '',
+        assetModuleFilename: 'images/[name][ext][query]'
+    },
+    target: ['web', 'es5'],
+    stats: { children: true },
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+        open: true
+    },
+    module: {
+        rules: [ // esto es un array de reglas
+            // añádele un objeto que contenga reglas para Babel
+            {
+                // una expresión regular que busca todos los archivos js
+                test: /\.js$/,
+                // todos los archivos deben ser procesados por babel-loader
+                loader: "babel-loader",
+                // excluye la carpeta node_modules, no necesitamos procesar archivos en ella
+                exclude: "/node_modules/"
             },
-          },
-          "postcss-loader",
-        ],
-      },
-      {
-        // añade la regla para el procesamiento de archivos
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin(
-      {
-        inject: true,
-        template: "./src/index.html",
-        filename: `index.html`,
-        chunks: ["app"],
-      } // ruta a nuestro archivo index.html}
-    ),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(), // conecta el plugin para fusionar archivos CSS
-  ],
-};
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    "postcss-loader"
+                ],
+            },
+            {
+                // añade la regla para el procesamiento de archivos
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: "asset/resource"
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+                inject: true,
+                template: "./src/index.html",
+                filename: `index.html`,
+                chunks: ['app']
+            } // ruta a nuestro archivo index.html}
+        ),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin() // conecta el plugin para fusionar archivos CSS
+    ]
+}
