1a3ba1df5eb8dda9bb3b0285f6e77517130151dc
mmgonnar <gn.mariela@gmail.com>
2024-02-16 16:39:28 -0600

    feat(javascript): Added getCards method to Api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,12 +3,22 @@ class Api {
     this._url = url;
     this._token = token;
   }
+
   getUserInfo() {
-    return fetch(this._url + '/user/me' {
+    return fetch(this._url + "/user/me", {
       headers: {
         Autorization: this._token,
         "Content-Type": "application/json",
       },
-    }).then(response => response.json())
+    }).then((response) => response.json());
+  }
+
+  getCards() {
+    return fetch(thus._url + "/cards", {
+      headers: {
+        Authorization: this._token,
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
   }
 }
