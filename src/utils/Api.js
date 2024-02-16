f7f1fe15551b47afd4fd511de44967ea2c782a88
mmgonnar <gn.mariela@gmail.com>
2024-02-16 13:03:54 -0600

    feat(javascript):

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,4 +3,12 @@ class Api {
     this._url = url;
     this._token = token;
   }
+  getUserInfo() {
+    return fetch(this._url + '/user/me' {
+      headers: {
+        Autorization: this._token,
+        "Content-Type": "application/json",
+      },
+    }).then(response => response.json())
+  }
 }
